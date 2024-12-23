2d7794299e20c257d98fed3959339b4f0be39bea
gaelbelami <ishimwegaelbelami@gmail.com>
2024-12-23 07:32:11 -0800

    add event management UI with create, edit, preview, and delete functionalities

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,11 @@ import { notFound } from "next/navigation";
 import prisma from "../lib/db";
 import { requireUser } from "../lib/hooks";
 import { EmptyState } from "../components/EmptyState";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ExternalLink, Link2, Pen, Settings, Trash, Users2 } from "lucide-react";
+import { Switch } from "@/components/ui/switch";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 async function getData(userId: string) {
   const data = await prisma.user.findUnique({
@@ -36,15 +41,61 @@ const data = await getData(session?.user?.id as string);
       { data.eventType.length === 0 ? (
         <EmptyState title="You have no Event" description="You can create your first event type by clicking the button below" buttonText="Add event type" href="/dashboard/new" />
       ) : (
-        <div>
-          {data.eventType.map((item) => (
-            <div key={item.id}>
-              <p>{item.title}</p>
-              <p>{item.url}</p>
-              <p>{item.duration}</p>
-            </div>
-          ))}
+        <>
+        <div className="flex items-center justify-between px-2">
+          <div className="hidden sm:grid gap-y-1">
+            <h1 className="text-3xl md:text-4xl font-bold">Event Types</h1>
+            <p className="text-muted-foreground">Create and manage your event types right here.</p>
+          </div>
+          <Button asChild>
+            <Link href="/dashboard/new">Create New Event</Link>
+          </Button>
         </div>
+        <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 mt-5">
+        {data.eventType.map((event) => (
+          <div key={event.id} className="items-center overflow-hidden shadow rounded-lg border relative">
+            <div className="absolute top-2 right-2">
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant='outline' size="icon">
+                    <Settings className="w-4 h-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>Event</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem asChild><Link href={`/${data.username}/${event.url}`}><ExternalLink className="mr-2 w-4 h-4"/>Preview</Link></DropdownMenuItem>
+                    <DropdownMenuItem asChild><Link href={`/${data.username}/${event.url}`}><Link2 className="mr-2 w-4 h-4"/>Copy</Link></DropdownMenuItem>
+                    <DropdownMenuItem asChild><Link href={`/${data.username}/${event.url}`}><Pen className="mr-2 w-4 h-4"/>Edit</Link></DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem asChild><Link href={`/${data.username}/${event.url}`} className="hover:bg-red-500/50"><Trash className="mr-2 w-4 h-4"/>Delete</Link></DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+            </div>
+            <Link href={`/dashboard/new/`} className="flex items-center justify-between  p-4">
+              <div className="flex-shrink-0"><Users2 className="w-5 h-5"/></div>
+              <div className="ml-5 w-0 flex-1">
+                <dl>
+                  <dt className="text-sm font-medium text-muted-foreground">
+                    {event.duration} Minutes Meeting
+                  </dt>
+                  <dd className="text-lg font-medium">{event.title}</dd>
+                </dl>
+              </div>
+            </Link>
+            <div className="bg-muted px-5 py-3 justify-between items-center flex">
+              <Switch />
+              <Button>
+                Edit Event
+              </Button>
+            </div>
+          </div>
+        ))}
+        </div>
+        </>
       )}
 
     </div>
